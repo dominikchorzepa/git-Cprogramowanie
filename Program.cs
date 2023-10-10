2d8eea23e4d37b69ad84c833a072e4f001f0ae61
Dominik Chorzepa <dominikchorzepa@wp.pl>
2023-10-10 22:55:14 +0200

    Zmiany

--- Program.cs
+++ Program.cs
@@ -1,4 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Zmiany...");
-Console.WriteLine("Zmian ciag dalszy.. :)");
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Zadanie 2.2
+        int a;
+        int b;
+        int c;
+        int delta;
+
+        Console.WriteLine("Obliczanie delty dla równania kwadratowego!");
+        Console.WriteLine("Podaj a: ");
+        a = int.Parse(Console.ReadLine());
+        Console.WriteLine("Podaj b: ");
+        b = int.Parse(Console.ReadLine());
+        Console.WriteLine("Podaj c: ");
+        c = int.Parse(Console.ReadLine());
+
+        delta = b * b - 4 * a * c;
+        Console.WriteLine($"Delta = {delta}");
+    }
+}
